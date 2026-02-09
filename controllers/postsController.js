9a3c9c839bf153e0244df9359df04441d36e9c65
Pietro Scurti <pietroscurti9@gmail.com>
2026-02-09 17:23:30 +0100

    esercizio completato

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -9,9 +9,9 @@ function index(req, res) {
 
 function show(req, res) {
 
-    const id = Number(req.params.id);
+    const id = Number(req.params.id); // 3
 
-    const post = posts.find(post => post.id === id);
+    const post = posts.find(post => post.id === id); // {} oopure undefined
 
     if (!post) {
         return res.status(404).send('Post non trovato');
@@ -19,7 +19,7 @@ function show(req, res) {
 
     res.json(post);
 
-    res.send('Dettagli del post ' + req.params.id);
+    
 }
 
 
@@ -39,7 +39,23 @@ function modify(req, res) {
 }
 
 function destroy(req, res) {
-    res.send('Eliminazione del post ' + req.params.id);
+    // res.send('Eliminazione del post ' + req.params.id);
+    const id = Number(req.params.id);
+
+    const post = posts.find(post => post.id === id);
+
+    if (!post) {
+        return res.status(404).send('Post non trovato');
+    }
+
+    // remove post from array
+    posts.splice(posts.indexOf(post), 1)
+
+    console.log(posts)
+
+    res.sendStatus(204)
+
+
 }
 
 module.exports = { index, show, store, update, modify, destroy }
